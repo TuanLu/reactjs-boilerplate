5927b8dda8198c6eb93cb447d82b47f2a1460d19
Tim <tuan.mrbean@gmail.com>
2020-09-30 23:29:12 +0700

    test config 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
+import Text from "./components/Text";
 
 function App() {
   return (
     <div className="main">
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
+      <img src={require("./assets/images/logo.png")} />
+      <Text />
     </div>
   );
 }
