14a458399106a6c1ae9d1c81814f3cabf009cd5e
Tim <tuan.mrbean@gmail.com>
2020-09-30 23:04:08 +0700

    test config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const TEST = 'NOT USING';
+const TEST = "NOT USING";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
