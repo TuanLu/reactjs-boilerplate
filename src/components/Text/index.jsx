c5c4898ab886e824231ee56657d3d7a0c05c01e2
Tuan Lu <tuanld@coccoc.com>
2020-10-01 10:23:38 +0700

    add custom font on local + google font

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Button() {
-  return <div className="sample-btn">Sample Text</div>;
+  return (
+    <div className="sample-btn">Almost before we knew it, we had left the ground</div>
+  );
 }
